973fd6b3bb6a92e728da1535bad25133dd658c6c
atesbugra18 <154465801+atesbugra18@users.noreply.github.com>
2025-04-21 23:15:33 +0300

    buğra kategori tasarımları yapıldı

---
+++ Kutuphane/ChildFormsKitap/KategoriSilDuzenle.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Kutuphane.ChildFormsKitap
+{
+    partial class KategoriSilDuzenle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grboxKategorisildüzenle = new System.Windows.Forms.GroupBox();
+            this.lblKategoriAdi = new System.Windows.Forms.Label();
+            this.txtKategoriadi = new System.Windows.Forms.TextBox();
+            this.chkKategoriadi = new System.Windows.Forms.CheckBox();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btndegistir = new System.Windows.Forms.Button();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.grboxKategorisildüzenle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grboxKategorisildüzenle
+            // 
+            this.grboxKategorisildüzenle.Controls.Add(this.btnclose);
+            this.grboxKategorisildüzenle.Controls.Add(this.btndegistir);
+            this.grboxKategorisildüzenle.Controls.Add(this.btnsil);
+            this.grboxKategorisildüzenle.Controls.Add(this.chkKategoriadi);
+            this.grboxKategorisildüzenle.Controls.Add(this.txtKategoriadi);
+            this.grboxKategorisildüzenle.Controls.Add(this.lblKategoriAdi);
+            this.grboxKategorisildüzenle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grboxKategorisildüzenle.Location = new System.Drawing.Point(0, 0);
+            this.grboxKategorisildüzenle.Name = "grboxKategorisildüzenle";
+            this.grboxKategorisildüzenle.Size = new System.Drawing.Size(890, 605);
+            this.grboxKategorisildüzenle.TabIndex = 0;
+            this.grboxKategorisildüzenle.TabStop = false;
+            this.grboxKategorisildüzenle.Text = "Kategori Sil && Düzenle";
+            // 
+            // lblKategoriAdi
+            // 
+            this.lblKategoriAdi.AutoSize = true;
+            this.lblKategoriAdi.Location = new System.Drawing.Point(12, 41);
+            this.lblKategoriAdi.Name = "lblKategoriAdi";
+            this.lblKategoriAdi.Size = new System.Drawing.Size(124, 26);
+            this.lblKategoriAdi.TabIndex = 0;
+            this.lblKategoriAdi.Text = "Kategori Adı:";
+            // 
+            // txtKategoriadi
+            // 
+            this.txtKategoriadi.Location = new System.Drawing.Point(142, 38);
+            this.txtKategoriadi.MaxLength = 25;
+            this.txtKategoriadi.Name = "txtKategoriadi";
+            this.txtKategoriadi.Size = new System.Drawing.Size(200, 33);
+            this.txtKategoriadi.TabIndex = 1;
+            // 
+            // chkKategoriadi
+            // 
+            this.chkKategoriadi.AutoSize = true;
+            this.chkKategoriadi.Location = new System.Drawing.Point(358, 40);
+            this.chkKategoriadi.Name = "chkKategoriadi";
+            this.chkKategoriadi.Size = new System.Drawing.Size(97, 30);
+            this.chkKategoriadi.TabIndex = 2;
+            this.chkKategoriadi.Text = "Değiştir";
+            this.chkKategoriadi.UseVisualStyleBackColor = true;
+            // 
+            // btnsil
+            // 
+            this.btnsil.Location = new System.Drawing.Point(710, 155);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(168, 37);
+            this.btnsil.TabIndex = 5;
+            this.btnsil.Text = "Sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            // 
+            // btndegistir
+            // 
+            this.btndegistir.Location = new System.Drawing.Point(520, 155);
+            this.btndegistir.Name = "btndegistir";
+            this.btndegistir.Size = new System.Drawing.Size(168, 37);
+            this.btndegistir.TabIndex = 6;
+            this.btndegistir.Text = "Değiştir";
+            this.btndegistir.UseVisualStyleBackColor = true;
+            // 
+            // btnclose
+            // 
+            this.btnclose.BackColor = System.Drawing.Color.Transparent;
+            this.btnclose.FlatAppearance.BorderSize = 0;
+            this.btnclose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnclose.Location = new System.Drawing.Point(838, 27);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(40, 40);
+            this.btnclose.TabIndex = 7;
+            this.btnclose.UseVisualStyleBackColor = false;
+            // 
+            // KategoriSilDuzenle
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(890, 605);
+            this.Controls.Add(this.grboxKategorisildüzenle);
+            this.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "KategoriSilDuzenle";
+            this.Text = "KategoriSilDuzenle";
+            this.grboxKategorisildüzenle.ResumeLayout(false);
+            this.grboxKategorisildüzenle.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grboxKategorisildüzenle;
+        private System.Windows.Forms.CheckBox chkKategoriadi;
+        private System.Windows.Forms.TextBox txtKategoriadi;
+        private System.Windows.Forms.Label lblKategoriAdi;
+        private System.Windows.Forms.Button btndegistir;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnclose;
+    }
+}
---
+++ Kutuphane/ChildFormsKitap/KategorileriListele.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Kutuphane.ChildFormsKitap
+{
+    partial class KategorileriListele
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grlistele = new System.Windows.Forms.GroupBox();
+            this.tablekategori = new System.Windows.Forms.DataGridView();
+            this.grlistele.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tablekategori)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grlistele
+            // 
+            this.grlistele.Controls.Add(this.tablekategori);
+            this.grlistele.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grlistele.Location = new System.Drawing.Point(0, 0);
+            this.grlistele.Name = "grlistele";
+            this.grlistele.Size = new System.Drawing.Size(890, 605);
+            this.grlistele.TabIndex = 0;
+            this.grlistele.TabStop = false;
+            this.grlistele.Text = "Kategorileri Listele";
+            // 
+            // tablekategori
+            // 
+            this.tablekategori.BackgroundColor = System.Drawing.Color.LightBlue;
+            this.tablekategori.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tablekategori.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tablekategori.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.tablekategori.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablekategori.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tablekategori.EnableHeadersVisualStyles = false;
+            this.tablekategori.GridColor = System.Drawing.Color.LightGray;
+            this.tablekategori.Location = new System.Drawing.Point(3, 29);
+            this.tablekategori.Name = "tablekategori";
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.AliceBlue;
+            this.tablekategori.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.tablekategori.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.White;
+            this.tablekategori.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tablekategori.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
+            this.tablekategori.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.LightBlue;
+            this.tablekategori.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.tablekategori.Size = new System.Drawing.Size(884, 573);
+            this.tablekategori.TabIndex = 0;
+            // 
+            // KategorileriListele
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(890, 605);
+            this.Controls.Add(this.grlistele);
+            this.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "KategorileriListele";
+            this.Text = "KategorileriListele";
+            this.grlistele.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tablekategori)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grlistele;
+        private System.Windows.Forms.DataGridView tablekategori;
+    }
+}
