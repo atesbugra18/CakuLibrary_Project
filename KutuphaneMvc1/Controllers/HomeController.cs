5dbec6a10d3e3f8f1b3b16bc476b1442e1c6c47a
atesbugra18 <154465801+atesbugra18@users.noreply.github.com>
2025-05-17 04:07:55 +0300

    Buğra: Mvc layout tasarlandı layouta session ayarlanacak diğer sayfalar tasarlanacak

--- KutuphaneMvc1/Controllers/HomeController.cs
+++ KutuphaneMvc1/Controllers/HomeController.cs
@@ -13,27 +13,6 @@ namespace KutuphaneMvc1.Controllers
         {
             return View();
         }
-        public async Task<ActionResult> ProfileImage(string id)
-        {
-            var url = "https://drive.google.com/uc?export=download&id=" + id;
-            System.Net.Http.HttpClient client = null;
-            try
-            {
-                client = new System.Net.Http.HttpClient();
-                var data = await client.GetByteArrayAsync(url);
-                return File(data, "image/jpeg");
-            }
-            catch
-            {
-                var def = Server.MapPath("~/Content/images/default-profile.png");
-                return File(System.IO.File.ReadAllBytes(def), "image/png");
-            }
-            finally
-            {
-                client?.Dispose();
-            }
-        }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
